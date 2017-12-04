fde6d282a158b682e63744ce14fcf2b42ee61265
ambrwlsn <arw89@uclive.ac.nz>
2017-12-04 09:37:45 +0000

    refactored JS

--- js/script.js
+++ js/script.js
@@ -1,134 +1,69 @@
-var oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "https://colorlovers.now.sh/api/palettes");
-oReq.send();
-
-// function formatDate(rawString) {
-//
-//   const d = new Date(rawString);
-//
-//   const day = d.getDate().toString();
-//   const month = (d.getMonth() + 1).toString();
-//   const yyyy = d.getFullYear();
-//
-//
-//   const dd = day.length === 1 ? '0' + day : day;
-//   const mm = month.length === 1 ? '0' + month : month;
-//
-//   return yyyy + '-' + mm + '-' + dd;
-// }
-
-function reqListener() {
-    const results = JSON.parse(this.responseText);
-    results.forEach(function(item) {
-
-        // set up variable to hold color box markup
-        // loop over all the colors
-        // for each color, add its markup to the box markup variable
-
-        // variable
-        // item.colors loop:
-        //   variable += 'individual box markup'
-
-        let colorboxMarkup = '';
-
-        var arrayLength = item.colors.length;
-        for (var i = 0; i < arrayLength; i++) {
-            console.log(item.colors[i]);
-            colorboxMarkup += `<div class='mini--box' style="background:#${item.colors[i]};"></div>`
-        }
-
-        // for (let value of item.colors) {
-        //   value += 1;
-        //   colorboxMarkup += `<div class='mini--box' style="background:#${item.colors[value]};"></div>`
-        // }
-
-        // var d = new Date(item.dateCreated);
-        // console.log('DATE OBJECT', d.getDate());
-        // var dateString = `${d.getDate()}-${d.getMonth()+1}-${d.getFullYear()}`;
-
-
-
-  var d = new Date(item.dateCreated);
-
-  var day = d.getDate().toString();
-  var month = (d.getMonth() + 1).toString();
-
-  var yyyy = d.getFullYear();
-
-  var dd = day.length === 1 ? '0' + day : day;
-  var mm = month.length === 1 ? '0' + month : month;
-
-  var dateString = `${yyyy}-${mm}-${dd}`;
-
-
-        let markup = `
-   <div class='card card-1'>
-   <div class='container'>
-
-     ${item.title}
-     <span class='end'>
-
-      ${dateString}
-      formatDate(item.dateCreated)
-
-     </span>
-     <p>
-       <span class='big-number'> ${item.numViews} </span> views &nbsp;
-       <span class='big-number'> ${item.numVotes} </span> votes &nbsp;
-       <span class='big-number'> ${item.numComments} </span> comments &nbsp;
-       <span class='big-number'> ${item.numHearts} </span> hearts &nbsp;
-     </p>
-     </div>
-     <div class='box'>
-      ${colorboxMarkup}
-       </div>
-     </div>`;
-        document.body.innerHTML += markup;
-    })
+function fetch_palettes() {
+    return fetch('https://colorlovers.now.sh/api/palettes').then(function __parse_colorlovers_json(response) {
+        return response.json();
+    }).catch(handle_api_error);
 }
 
-// const arr = ['a', 'b', 'c'];
-//
-// arr.forEach(function(element) {
-//     console.log(element);
-// });
+function handle_api_error(err) {
+    console.error('API Error', err);
+    alert('The colorlovers API seems to have some problems. Please try again later');
+}
 
+function format_date(raw_date) {
+    const parsed_date = new Date(raw_date);
+    const date_options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    return parsed_date.toLocaleDateString('en-gb', date_options);
+}
 
-// I want to loop through the 'colors' array
-// I want to place each hex value in the array into a predefined box
-// I want the 'background' property to be populated by the hex codes
+function create_palettes_markup(palettes) {
+    return palettes.map(create_palette_markup);
+}
 
-// const palette = {
-//   name: "Awesome Palette",
-//   date: Date.now()
-// };
+function insert_palettes(palettes_markup) {
+    const palettes_fragment = document.createDocumentFragment();
+    palettes_markup.forEach(function __append_markup_to_docfrag(markup) {
+        let container_element = document.createElement('span');
+        container_element.insertAdjacentHTML('afterbegin', markup);
+        palettes_fragment.appendChild(container_element.firstElementChild);
+    })
+    document.body.appendChild(palettes_fragment);
+}
 
-// const palettes = [
-//   {
-//     name: "Awesome Palette",
-//     date: Date.now()
-//   },
-//   {
-//     name: "Awesome Palette",
-//     date: Date.now()
-//   },
-//   {
-//     name: "Awesome Palette",
-//     date: Date.now()
-//   },
-//   {
-//     name: "Ok Palette",
-//     date: Date.now()
-//   }
-// ]
+function create_colourbox_markup(colour) {
+    return `<div class='mini--box' style='background: #${colour};'></div>`
+}
 
-// for (var i = 0; i < item.colors; i++) {
-//   let markup = `
-//     <div class='card card-1'>
-//       ${item.colors[i]}
-//       </span>
-//     </div>`;
-//
-//   document.body.innerHTML += markup;
-// }
+function create_palette_markup(palette) {
+    const colorbox_markup = palette.colors.map(create_colourbox_markup).join('');
+    const palette_date_created = format_date(palette.dateCreated);
+    const markup = `
+        <div class='card card-1'>
+           <div class='container'>
+                ${palette.title}
+                <span class='end'>${palette_date_created}</span>
+                <p>
+                    <span class='big-number'> ${palette.numViews} </span> views &nbsp;
+                    <span class='big-number'> ${palette.numVotes} </span> votes &nbsp;
+                    <span class='big-number'> ${palette.numComments} </span> comments &nbsp;
+                    <span class='big-number'> ${palette.numHearts} </span> hearts &nbsp;
+                </p>
+            </div>
+            <div class='box'>${colorbox_markup}</div>
+        </div>`;
+    return markup;
+}
+
+(function __init() {
+    // fetch the data from the API & execute `__create_palettes_markup_and_insert` once the data is returned & no error happened
+    fetch_palettes().then(function __create_palettes_markup_and_insert(palettes) {
+        // create the markup for our palettes
+        const palettes_markup = create_palettes_markup(palettes)
+        // insert the created markup into our DOM as children of the <body> element
+        insert_palettes(palettes_markup)
+    })
+})()
