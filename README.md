2243cc723a3f1968ace3a145310b41b3ce16efd1
ambrwlsn <arw89@uclive.ac.nz>
2020-09-19 19:35:04 +0200

    Update to use Fetch and a non-broken color API

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # colour-lovahs
-**Creating a functional version of below image using https://colorlovers.now.sh/api/palettes**
-![colour lovers task](https://github.com/ambrwlsn/colour-lovahs/blob/master/img/colour-lovah.png)
+**Refresh the page for a fresh colour palette!**
+
+Built with the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API">Fetch API</a>, a <a href="http://colormind.io/api-access/">colour palette API</a>, and JS-injected markup.
+
+I wanted to host the result on Netlify but the colour palette API only offers HTTP and that's no good.
+
+So if you want to play with the site, please clone this repository and view the index file in a browser, or run `php -S localhost:8001` in your terminal.
+
+![colour lovahs result](./img/colour_lovahs.png)
--- js/script.js
+++ js/script.js
@@ -1,69 +1,47 @@
-function fetch_palettes() {
-    return fetch('https://colorlovers.now.sh/api/palettes').then(function __parse_colorlovers_json(response) {
-        return response.json();
-    }).catch(handle_api_error);
-}
+const handleApiError = error => {
+  console.error('API Error', error);
+  alert(
+    'The colormind API seems to have some problems. Please try again later'
+  );
+};
 
-function handle_api_error(err) {
-    console.error('API Error', err);
-    alert('The colorlovers API seems to have some problems. Please try again later');
-}
+const fetchPalettes = () => {
+  return fetch('http://colormind.io/api/', {
+    body: '{"model":"default"}',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    method: 'POST'
+  })
+    .then(response => response.json())
+    .catch(handleApiError);
+};
 
-function format_date(raw_date) {
-    const parsed_date = new Date(raw_date);
-    const date_options = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    };
-    return parsed_date.toLocaleDateString('en-gb', date_options);
-}
-
-function create_palettes_markup(palettes) {
-    return palettes.map(create_palette_markup);
-}
-
-function insert_palettes(palettes_markup) {
-    const palettes_fragment = document.createDocumentFragment();
-    palettes_markup.forEach(function __append_markup_to_docfrag(markup) {
-        let container_element = document.createElement('span');
-        container_element.insertAdjacentHTML('afterbegin', markup);
-        palettes_fragment.appendChild(container_element.firstElementChild);
+const createPaletteMarkup = data => {
+  JSON.stringify(data);
+  const colours = data.result.map(colour => colour);
+  const rgbValues = colours.map(colour => `rgb(${colour})`);
+  const headingColour = `rgb(${colours[Math.floor(Math.random() * 4) + 0]})`;
+  console.log(headingColour);
+  const boxes = rgbValues
+    .map(value => {
+      return `<div class='card' style="background-color:${value}"></div>`;
     })
-    document.body.appendChild(palettes_fragment);
-}
+    .join('');
 
-function create_colourbox_markup(colour) {
-    return `<div class='mini--box' style='background: #${colour};'></div>`
-}
+  const paletteMarkup = `<h1 style="color:${headingColour}">Welcome to Colour Lovahs</h1><div class='container'>${boxes}</div>`;
 
-function create_palette_markup(palette) {
-    const colorbox_markup = palette.colors.map(create_colourbox_markup).join('');
-    const palette_date_created = format_date(palette.dateCreated);
-    const markup = `
-        <div class='card card-1'>
-           <div class='container'>
-                ${palette.title}
-                <span class='end'>${palette_date_created}</span>
-                <p>
-                    <span class='big-number'> ${palette.numViews} </span> views &nbsp;
-                    <span class='big-number'> ${palette.numVotes} </span> votes &nbsp;
-                    <span class='big-number'> ${palette.numComments} </span> comments &nbsp;
-                    <span class='big-number'> ${palette.numHearts} </span> hearts &nbsp;
-                </p>
-            </div>
-            <div class='box'>${colorbox_markup}</div>
-        </div>`;
-    return markup;
-}
+  return paletteMarkup;
+};
 
-(function __init() {
-    // fetch the data from the API & execute `__create_palettes_markup_and_insert` once the data is returned & no error happened
-    fetch_palettes().then(function __create_palettes_markup_and_insert(palettes) {
-        // create the markup for our palettes
-        const palettes_markup = create_palettes_markup(palettes)
-        // insert the created markup into our DOM as children of the <body> element
-        insert_palettes(palettes_markup)
-    })
-})()
+const insertPalettes = palettesMarkup => {
+  document.body.insertAdjacentHTML('afterbegin', palettesMarkup);
+};
+
+(() => {
+  fetchPalettes().then(data => {
+    const palettesMarkup = createPaletteMarkup(data);
+    // insert the created markup into our DOM as children of the <body> element
+    insertPalettes(palettesMarkup);
+  });
+})();
